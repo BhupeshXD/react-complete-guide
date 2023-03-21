fa83c48aa4d6ef4e363b2548e174672eb766124d
BhupeshXD <bhupeshdhapola7@gmail.com>
2023-03-21 19:33:56 +0530

    21 feb

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,16 @@
-function ExpenseItem(){
-    return <h2>Expense item!</h2>
+import './ExpenseItem.css';
+
+function ExpenseItem() {
+  return (
+    <div className="expense-item">
+      <div>March 21th 2023</div>
+      <div className=
+      "expense-item-description">
+        <h2>Car Insurance</h2>
+      </div>
+      <div className="expense-item_price">₹932.23</div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
