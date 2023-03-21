875be4078a2422cf907750a72429e4fa1d5c35d2
BhupeshXD <bhupeshdhapola7@gmail.com>
2023-03-21 13:05:00 +0530

    21 feb

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
     <div>
       <h2>Let's get started</h2>
+      <p>This is so visible</p>
+      <ExpenseItem></ExpenseItem>    
     </div>
   );
 }
